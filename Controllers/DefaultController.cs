c9543c3f15aa1bb338275d33231ba80a6d35e55f
tanhadel <taherizabiolla00@gmail.com>
2024-03-09 14:18:52 +0100

    first

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiliconAssignemt_3.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    let btnMenu= document.querySelector('.btm-menu')
+    let nav = document.querySelector ('.nav')
+
+    btnMenu.addEventListener ('.click', ()=>{
+        btnMenu.classList.toggle('active')
+        btnMenu.classList.toggle('fixed')
+
+        nav.classList.toggle ('active')
+
+    })
+
+    window.addEventListener('resize', ()=>{
+        btnMenu.classList.remove('active')
+        btnMenu.classList.remove('fixed')
+
+        nav.classList.remove('active')
+
+    })
+})
